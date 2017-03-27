8bcf971c176de4814368e5cee7e004d3395eadf5
EC2 Default User <ec2-user@ip-172-31-28-238.ec2.internal>
2017-03-27 22:32:48 +0000

    changed port mapping to port 80 which I am using, changed run script to only run when last command succeeds

--- swarm_run.sh
+++ swarm_run.sh
@@ -2,8 +2,19 @@
 
 cd thrones_db_spring
 mvn clean package
+if [[ $? != 0 ]]
+then
+	cd ..
+	return
+fi
 docker build -t thrones_db_spring .
 cd ..
+
+if [[ $? != 0 ]]
+then
+        return
+fi
+
 
 docker swarm init
 docker deploy --compose-file swarm_compose.yml thrones_db
